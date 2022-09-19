8d430ddaf06adea737c4d77dc331ef2aa673bf0d
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-09-19 19:32:53 +0700

    Praktikum dan Screenshots Event Handling

---
+++ 13_Event Handling/praktikum/eventhandling/src/App.jsx
@@ -0,0 +1,11 @@
+import TodoApp from "./Todos/TodoApp";
+
+function App() {
+    return (
+        <div className="App">
+            <TodoApp />
+        </div>
+    );
+}
+
+export default App;
---
+++ 13_Event Handling/praktikum/eventhandling/src/Todos/Data.jsx
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Mengerjakan Exercise",
+        "completed": true
+    },
+    {
+        "id": 2,
+        "title": "Mengerjakan Assignment",
+        "completed": false
+    }
+]
